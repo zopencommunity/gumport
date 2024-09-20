21a066fc3875fa5a6778e1df86bb58e3be63775e
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:01 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gumport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/gumport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gumport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection of UI elements to use in your shell scripts' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/gumport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection of UI elements to use in your shell scripts' )]
   }
 }
